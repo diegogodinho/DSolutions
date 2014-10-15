fbe00c102c4e55c66344acc7a36a2fa307a0ffa2
diegogodinho <diegogodinho.ferreira@yahoo.com.br>
2014-10-15 10:18:22 -0300

    Teste

--- DSolutionWeb/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSolutionWeb.MvcApplication" Language="C#" %>
--- WebApiAngularPrjWeb/js/angular/controllers.js
+++
@@ -1,9 +0,0 @@
-var controllerPadrao = angular.module('controllerPadrao', []);
-
-controllerPadrao.controller('PrincipalCtrl', ['$scope', '$location', '$http',
-  function ($scope, $location, $http) {
-      $scope.CaminhoListaUsuario = '/Usuario';
-      $scope.CaminhoCadastroCategoriaPrd = '/Usuario';
-      $scope.CaminhoCadastroPrd = '/Usuario';
-  } ]);
-
